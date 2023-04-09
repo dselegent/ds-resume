935a5f3739bc70c5be8d85dc5ff5f61ac1fb6112
dselegent <dselegent@qq.com>
2023-04-09 22:24:05 +0800

    chore: 添加一些方便开发的配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,52 @@
-import react from '@vitejs/plugin-react'
-import Unocss from 'unocss/vite'
-import { fileURLToPath } from 'url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { fileURLToPath } from 'node:url'
+import { wrapperEnv } from './build/utils'
+import createProxy from './build/vite/proxy'
+import createBuild from './build/vite/build'
+import createVitePlugins from './build/vite/plugins'
+import autoprefixer from 'autoprefixer'
 
-export default defineConfig({
-  plugins: [react(), Unocss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build' // 是否是构建 serve
+  const env = loadEnv(mode, process.cwd()) // 加载env环境
+  const viteEnv = wrapperEnv(env)
+
+  return {
+    plugins: createVitePlugins(),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    css: {
+      postcss: {
+        // 关键代码
+        plugins: [
+          autoprefixer({
+            // 自动添加前缀
+            overrideBrowserslist: [
+              'Android 4.1',
+              'iOS 7.1',
+              'Chrome > 31',
+              'ff > 31',
+              'ie >= 8',
+              //'last 2 versions', // 所有主流浏览器最近2个版本
+            ],
+            grid: true,
+          }),
+        ],
+      },
+    },
+    server: {
+      port: 8888, //启动端口
+      host: '0.0.0.0',
+      hmr: {
+        host: 'localhost',
+        port: 8080,
+      },
+      // 设置代理
+      proxy: createProxy(),
+    },
+    build: createBuild(viteEnv),
+  }
 })
