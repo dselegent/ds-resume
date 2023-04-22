b6f658a1cb87d77eae13346527f25fe987cb1711
dselegent <dselegent@qq.com>
2023-04-22 17:29:07 +0800

    fix: 修复头部被其他定位元素覆盖

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -17,7 +17,7 @@ const NavBar: React.FC = () => {
   const [isLogin] = useBoolean(true)
 
   return (
-    <section className='fixed h-16 w-full f-b-c bg-white px-15'>
+    <section className='fixed z-10 h-16 w-full f-b-c bg-[rgba(255,255,255,.95)] px-15'>
       <Link to='/' className='h-full flex items-center'>
         <img className='w-8' src={logo} alt='logo' />
         <span className="ml-3 font-['monospace'] text-2xl text-font_color letter-2">ds化简</span>
