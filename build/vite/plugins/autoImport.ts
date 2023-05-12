858bd790c913fb70818107f41df51d7a79a64ca7
dselegent <dselegent@qq.com>
2023-05-12 20:49:11 +0800

    ci: 自动导入新增 src/options 路径

--- build/vite/plugins/autoImport.ts
+++ build/vite/plugins/autoImport.ts
@@ -7,7 +7,15 @@ export default function configAutoImportPlugin() {
       /\.md$/, // .md
     ],
     imports: ['react', 'react-router-dom', 'ahooks'],
-    dirs: ['src/store/**/*', 'src/router/**/*', 'src/components/**/*', 'src/pages/**/*', 'src/hooks', 'src/utils'],
+    dirs: [
+      'src/store/**/*',
+      'src/router/**/*',
+      'src/components/**/*',
+      'src/pages/**/*',
+      'src/options',
+      'src/hooks',
+      'src/utils',
+    ],
     dts: './types/auto-imports.d.ts',
     // 生成全局声明文件，给eslint用
     eslintrc: {
