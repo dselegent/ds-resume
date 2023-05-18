ad356e17bf74cc79e2ed646870e10d67ca723a11
dselegent <dselegent@qq.com>
2023-05-18 23:47:17 +0800

    feat: 使用 Emotion 修改 Skeleton.Image 默认样式

--- src/components/SkeletonCard/index.tsx
+++ src/components/SkeletonCard/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Skeleton } from 'antd'
 
+const SkeletonImage = styled(Skeleton.Image)`
+  :where(.css-dev-only-do-not-override-ipqtsk).ant-skeleton.ant-skeleton-element .ant-skeleton-image {
+    width: 300px;
+    height: 400px;
+  }
+`
+
 const SkeletonCard: React.FC = () => {
   return (
     <section className='mb-10 h-110 w-75 flex basis-[fit-content] flex-col overflow-hidden rounded'>
-      <Skeleton.Image
-        active
-        style={{
-          width: '300px',
-          height: '400px',
-        }}
-      />
+      <SkeletonImage active />
       <Skeleton active title={{ width: '100%' }} />
     </section>
   )
