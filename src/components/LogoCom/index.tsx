bc8d37f65a21e4f00200d7d3a022c6f15b846b16
dselegent <dselegent@qq.com>
2023-05-19 18:28:45 +0800

    feat: 使用 Emotion 动态修改 logo 字体颜色

--- src/components/LogoCom/index.tsx
+++ src/components/LogoCom/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import logo from '@/assets/logo.png'
 
+const LogoTextSapn = styled.span<{
+  fontColor: string
+}>`
+  color: ${props => props.fontColor};
+`
+
 type NavBarPropsType = { fontColor: string }
 
 const LogoCom: React.FC<NavBarPropsType> = ({ fontColor }) => {
   return (
     <Link to='/' className='h-full flex items-center'>
       <img className='w-9' src={logo} alt='logo' />
-      <span
-        className={
-          'ml-3 font-[cursive] text-2xl font-600 letter-2 transition-colors text-font_color ' + `text-${fontColor}`
-        }
-      >
+      <LogoTextSapn className='ml-3 font-[cursive] text-2xl font-600 transition-colors letter-2' fontColor={fontColor}>
         ds化简
-      </span>
+      </LogoTextSapn>
     </Link>
   )
 }
