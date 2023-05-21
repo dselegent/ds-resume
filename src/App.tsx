2a52f669a5ec0d396be205b5e93bc387ad77c173
dselegent <dselegent@qq.com>
2023-05-21 20:40:11 +0800

    style: 调整导入第三方包的顺序

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+
 import { BrowserRouter } from 'react-router-dom'
+import Router from '@/routers'
+
 import { ConfigProvider } from 'antd'
+import theme from '@/styles/antd-theme'
 import zhCn from 'antd/locale/zh_CN'
 import 'dayjs/locale/zh-cn'
-import theme from '@/styles/antd-theme'
-import Router from '@/routers'
 
 export default function App() {
   return (
