70ee98a41939122502570a317607d22dfeea2502
dselegent <dselegent@qq.com>
2023-05-21 16:19:17 +0800

    chore: antd 语言包修改为中文

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
+import zhCn from 'antd/locale/zh_CN'
 import theme from '@/styles/antd-theme'
 import Router from '@/routers'
 
 export default function App() {
   return (
-    <ConfigProvider theme={theme}>
+    <ConfigProvider theme={theme} locale={zhCn}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
