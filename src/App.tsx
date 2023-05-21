d665199379ae98a2cf43d378ac5b3072f297560b
dselegent <dselegent@qq.com>
2023-05-21 20:38:57 +0800

    feat: datePicker 组件中文化

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { ConfigProvider } from 'antd'
 import zhCn from 'antd/locale/zh_CN'
+import 'dayjs/locale/zh-cn'
 import theme from '@/styles/antd-theme'
 import Router from '@/routers'
 
