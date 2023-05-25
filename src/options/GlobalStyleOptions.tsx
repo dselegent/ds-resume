6c18b18436e34b38f0eb8947050fca5f6b7a4a8e
dselegent <dselegent@qq.com>
2023-05-25 22:34:24 +0800

    feat: 收集全局样式到 store ，并同步到本地

--- src/options/GlobalStyleOptions.tsx
+++ src/options/GlobalStyleOptions.tsx
@@ -56,48 +56,47 @@ type GlobalStyleOptionsType = {
 }
 
 const GlobalStyleOptions: React.FC<GlobalStyleOptionsType> = ({ configShowStatus }) => {
-  const [form] = Form.useForm()
-  // 全局主题设置
-  const globalStyleOptionsForm = useReactive({
-    themeColor: '#079cfa',
-    fontFamily: '微软雅黑',
-    firstTitleFontSize: '12px',
-    secondTitleFontSize: '14px',
-    textFontSize: '14px',
-    secondTitleColor: '#666',
-    textFontColor: '#757575',
-    secondTitleWeight: 600,
-    textFontWeight: 500,
-    countModel: false,
-    marginTop: 0,
-    marginBottom: 45,
-    paddingTop: 0,
-    paddingBottom: 30,
-    paddingX: 50,
+  const dispatch = useAppDispatch()
+
+  // 计数器模式
+  const [countModel, setCountModel] = useLocalStorageState<boolean>('countModel', {
+    defaultValue: true,
   })
+
+  // 全局样式设置
+  const GLOBAL_STYLE = useAppSelector(selectorResumeJsonData).GLOBAL_STYLE
+  // 更新全局样式
+  const handleChangeGlobalStyle = (key: string, value: any) =>
+    dispatch(
+      changeResumeJsonGlobalStyleData({
+        key,
+        value,
+      })
+    )
 
   return (
     <section className='p-5'>
-      <Form form={form} labelCol={{ span: configShowStatus ? 6 : 10 }} labelAlign='left'>
+      <Form labelCol={{ span: configShowStatus ? 6 : 10 }} labelAlign='left'>
         <Form.Item label='主题颜色'>
           <ColorPicker
-            color={globalStyleOptionsForm.themeColor}
-            getColor={(value: string) => (globalStyleOptionsForm.themeColor = value)}
+            color={GLOBAL_STYLE.themeColor}
+            getColor={(value: string) => handleChangeGlobalStyle('themeColor', value)}
           />
         </Form.Item>
         <Form.Item label='字体选择'>
           <Select
             size='small'
             options={fontFamilyList}
-            value={globalStyleOptionsForm.fontFamily}
-            onChange={(value: string) => (globalStyleOptionsForm.fontFamily = value)}
+            value={GLOBAL_STYLE.fontFamily}
+            onChange={(value: string) => handleChangeGlobalStyle('fontFamily', value)}
+            placeholder='请选择字体'
           />
         </Form.Item>
         <Form.Item label='模块标题字体大小'>
           <Select
             size='small'
-            value={globalStyleOptionsForm.firstTitleFontSize}
-            onChange={(value: string) => (globalStyleOptionsForm.firstTitleFontSize = value)}
+            value={GLOBAL_STYLE.firstTitleFontSize}
+            onChange={(value: string) => handleChangeGlobalStyle('firstTitleFontSize', value)}
           >
             {fontSizeList.map((value: string) => (
               <Select.Option key={value} value={value} label={value}>
@@ -109,8 +108,8 @@ const GlobalStyleOptions: React.FC<GlobalStyleOptionsType> = ({ configShowStatus
         <Form.Item label='二级标题字体大小'>
           <Select
             size='small'
-            value={globalStyleOptionsForm.secondTitleFontSize}
-            onChange={(value: string) => (globalStyleOptionsForm.secondTitleFontSize = value)}
+            value={GLOBAL_STYLE.secondTitleFontSize}
+            onChange={(value: string) => handleChangeGlobalStyle('secondTitleFontSize', value)}
           >
             {fontSizeList.map((value: string) => (
               <Select.Option key={value} value={value} label={value}>
@@ -122,8 +121,8 @@ const GlobalStyleOptions: React.FC<GlobalStyleOptionsType> = ({ configShowStatus
         <Form.Item label='正文字体大小'>
           <Select
             size='small'
-            value={globalStyleOptionsForm.textFontSize}
-            onChange={(value: string) => (globalStyleOptionsForm.textFontSize = value)}
+            value={GLOBAL_STYLE.textFontSize}
+            onChange={(value: string) => handleChangeGlobalStyle('textFontSize', value)}
           >
             {fontSizeList.map((value: string) => (
               <Select.Option key={value} value={value} label={value}>
@@ -134,21 +133,21 @@ const GlobalStyleOptions: React.FC<GlobalStyleOptionsType> = ({ configShowStatus
         </Form.Item>
         <Form.Item label='二级标题颜色'>
           <ColorPicker
-            color={globalStyleOptionsForm.secondTitleColor}
-            getColor={(value: string) => (globalStyleOptionsForm.secondTitleColor = value)}
+            color={GLOBAL_STYLE.secondTitleColor}
+            getColor={(value: string) => handleChangeGlobalStyle('secondTitleColor', value)}
           />
         </Form.Item>
         <Form.Item label='正文字体颜色'>
           <ColorPicker
-            color={globalStyleOptionsForm.textFontColor}
-            getColor={(value: string) => (globalStyleOptionsForm.textFontColor = value)}
+            color={GLOBAL_STYLE.textFontColor}
+            getColor={(value: string) => handleChangeGlobalStyle('textFontColor', value)}
           />
         </Form.Item>
         <Form.Item label='二级标题字体粗细'>
           <Select
             size='small'
-            value={globalStyleOptionsForm.secondTitleWeight}
-            onChange={(value: number) => (globalStyleOptionsForm.secondTitleWeight = value)}
+            value={GLOBAL_STYLE.secondTitleWeight}
+            onChange={(value: number) => handleChangeGlobalStyle('secondTitleWeight', value)}
           >
             {fontWeightList.map((value: number) => (
               <Select.Option key={value} value={value} label={value}>
@@ -160,8 +159,8 @@ const GlobalStyleOptions: React.FC<GlobalStyleOptionsType> = ({ configShowStatus
         <Form.Item label='正文字体粗细'>
           <Select
             size='small'
-            value={globalStyleOptionsForm.textFontWeight}
-            onChange={(value: number) => (globalStyleOptionsForm.textFontWeight = value)}
+            value={GLOBAL_STYLE.textFontWeight}
+            onChange={(value: number) => handleChangeGlobalStyle('textFontWeight', value)}
           >
             {fontWeightList.map((value: number) => (
               <Select.Option key={value} value={value} label={value}>
@@ -174,85 +173,81 @@ const GlobalStyleOptions: React.FC<GlobalStyleOptionsType> = ({ configShowStatus
           <span className='text-sm font-700 text-stone-700 letter-3'>间距调整</span>
         </Divider>
         <Form.Item label='计数器模式'>
-          <Switch
-            size='small'
-            onChange={(value: boolean) => (globalStyleOptionsForm.countModel = value)}
-            checked={globalStyleOptionsForm.countModel}
-          />
+          <Switch size='small' onChange={(value: boolean) => setCountModel(value)} checked={countModel} />
         </Form.Item>
         <Form.Item label='模块上外边距'>
           <AntdSlider
-            countModel={globalStyleOptionsForm.countModel}
-            value={globalStyleOptionsForm.marginTop}
-            onChange={(value: number) => (globalStyleOptionsForm.marginTop = value)}
+            countModel={countModel}
+            value={GLOBAL_STYLE.modelMarginTop}
+            onChange={(value: number) => handleChangeGlobalStyle('modelMarginTop', value)}
             max={100}
             min={-100}
           />
           <AntdInputNumber
-            countModel={globalStyleOptionsForm.countModel}
+            countModel={countModel}
             size='small'
-            value={globalStyleOptionsForm.marginTop}
-            onChange={(value: any) => (globalStyleOptionsForm.marginTop = value)}
+            value={GLOBAL_STYLE.modelMarginTop}
+            onChange={(value: any) => handleChangeGlobalStyle('modelMarginTop', value)}
           />
         </Form.Item>
         <Form.Item label='模块下外边距'>
           <AntdSlider
-            countModel={globalStyleOptionsForm.countModel}
-            value={globalStyleOptionsForm.marginBottom}
-            onChange={(value: number) => (globalStyleOptionsForm.marginBottom = value)}
+            countModel={countModel}
+            value={GLOBAL_STYLE.modelMarginBottom}
+            onChange={(value: number) => handleChangeGlobalStyle('modelMarginBottom', value)}
             max={100}
             min={-100}
           />
           <AntdInputNumber
-            countModel={globalStyleOptionsForm.countModel}
+            countModel={countModel}
             size='small'
-            value={globalStyleOptionsForm.marginBottom}
-            onChange={(value: any) => (globalStyleOptionsForm.marginBottom = value)}
+            value={GLOBAL_STYLE.modelMarginBottom}
+            onChange={(value: any) => handleChangeGlobalStyle('modelMarginBottom', value)}
           />
         </Form.Item>
         <Form.Item label='模块上内边距'>
           <AntdSlider
-            countModel={globalStyleOptionsForm.countModel}
-            value={globalStyleOptionsForm.paddingTop}
-            onChange={(value: number) => (globalStyleOptionsForm.paddingTop = value)}
+            countModel={countModel}
+            value={GLOBAL_STYLE.pTop}
+            onChange={(value: number) => handleChangeGlobalStyle('pTop', value)}
             max={100}
             min={-100}
           />
           <AntdInputNumber
-            countModel={globalStyleOptionsForm.countModel}
+            countModel={countModel}
             size='small'
-            value={globalStyleOptionsForm.paddingTop}
-            onChange={(value: any) => (globalStyleOptionsForm.paddingTop = value)}
+            value={GLOBAL_STYLE.pTop}
+            onChange={(value: any) => handleChangeGlobalStyle('pTop', value)}
           />
         </Form.Item>
         <Form.Item label='模块下内边距'>
           <AntdSlider
-            countModel={globalStyleOptionsForm.countModel}
-            value={globalStyleOptionsForm.paddingBottom}
-            onChange={(value: number) => (globalStyleOptionsForm.paddingBottom = value)}
+            countModel={countModel}
+            value={GLOBAL_STYLE.pBottom}
+            onChange={(value: number) => handleChangeGlobalStyle('pBottom', value)}
             max={100}
             min={-100}
           />
           <AntdInputNumber
-            countModel={globalStyleOptionsForm.countModel}
+            countModel={countModel}
             size='small'
-            value={globalStyleOptionsForm.paddingBottom}
-            onChange={(value: any) => (globalStyleOptionsForm.paddingBottom = value)}
+            value={GLOBAL_STYLE.pBottom}
+            onChange={(value: any) => handleChangeGlobalStyle('pBottom', value)}
           />
         </Form.Item>
         <Form.Item label='模块左右内边距'>
           <AntdSlider
-            countModel={globalStyleOptionsForm.countModel}
-            value={globalStyleOptionsForm.paddingX}
-            onChange={(value: number) => (globalStyleOptionsForm.paddingX = value)}
+            countModel={countModel}
+            value={GLOBAL_STYLE.pLeftRight}
+            onChange={(value: number) => handleChangeGlobalStyle('pLeftRight', value)}
             max={100}
             min={-100}
           />
           <AntdInputNumber
-            countModel={globalStyleOptionsForm.countModel}
+            countModel={countModel}
             size='small'
-            value={globalStyleOptionsForm.paddingX}
-            onChange={(value: any) => (globalStyleOptionsForm.paddingX = value)}
+            value={GLOBAL_STYLE.pLeftRight}
+            onChange={(value: any) => handleChangeGlobalStyle('pLeftRight', value)}
           />
         </Form.Item>
       </Form>
