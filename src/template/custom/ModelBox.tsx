df72fe0a0d94248fb9ed4b746d92d8dc2f1b8762
dselegent <dselegent@qq.com>
2023-06-11 21:51:33 +0800

    feat: 初步完成简历模块拖拽排序

--- src/template/custom/ModelBox.tsx
+++ src/template/custom/ModelBox.tsx
@@ -1,19 +1,48 @@
 import React from 'react'
 import type { IMATERIALITEM } from '@/interface/material'
-
 import { Tooltip } from 'antd'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import type { Transform } from '@dnd-kit/utilities'
 
 type ModelBoxPropsType = {
   components: any
   item: IMATERIALITEM
+  id: string
 }
 
-const ModelBox: React.FC<ModelBoxPropsType> = ({ components, item }) => {
+const SortableItem = styled.div<{
+  transform: Transform | null
+  transition: string | undefined
+}>`
+  transform: ${props => CSS.Transform.toString(props.transform)};
+  transition: ${props => props.transition};
+`
+
+const ModelBox: React.FC<ModelBoxPropsType> = ({ components, item, id }) => {
+  // 拖拽排序
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id,
+  })
+
+  // // 选中的模块数据
+  const selectMaterial = useAppSelector(selectorSelectMaterial)
+
   // 移入的组件id
   const [hoverId, setHoverId] = useSafeState('')
 
   return (
-    <>
+    <SortableItem
+      ref={setNodeRef}
+      transform={transform}
+      transition={transition}
+      {...attributes}
+      {...listeners}
+      // className={'f-b-c border border-transparent border-dashed px-3 cursor-row-resize	hover:border-primary'}
+      style={{
+        background: selectMaterial.cptKeyId === item.keyId ? 'rgba(227, 231, 234, 0.6)' : '',
+      }}
+    >
       {item.show && (
         <section
           className='relative border border-transparent border-dashed transition-base'
@@ -23,7 +52,7 @@ const ModelBox: React.FC<ModelBoxPropsType> = ({ components, item }) => {
           onBlur={() => void 0}
         >
           {/* 模块操作区域 */}
-          <aside className={`absolute right-0  -top-8 ${hoverId === item.keyId ? 'block' : 'display-none'}`}>
+          <aside className={`absolute right-0 -top-8 ${hoverId === item.keyId ? 'block' : 'display-none'}`}>
             <Tooltip title='删除当前模块' placement='bottom'>
               <div className='h-8 w-8 f-c-c cursor-pointer rounded bg-emerald-500 hover:bg-emerald-400'>
                 <i className='i-ant-design-delete-outlined text-xl text-white'></i>
@@ -39,7 +68,7 @@ const ModelBox: React.FC<ModelBoxPropsType> = ({ components, item }) => {
           />
         </section>
       )}
-    </>
+    </SortableItem>
   )
 }
 
--- src/template/custom/index.tsx
+++ src/template/custom/index.tsx
@@ -1,20 +1,52 @@
 import React from 'react'
-import MaterialComponents from '@/utils/registerMaterialCom' // 所有物料组件
+import type { IMATERIALITEM } from '@/interface/material'
 import ModelBox from './ModelBox'
+import MaterialComponents from '@/utils/registerMaterialCom' // 所有物料组件
+import { DndContext } from '@dnd-kit/core'
+import type { DragEndEvent } from '@dnd-kit/core'
+import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 const Custom: React.FC = () => {
+  const dispatch = useAppDispatch()
+
   // 简历数据
   const resumeJsonData = useAppSelector(selectorResumeJsonData)
+
+  // 拖拽更新
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event
+
+    if (active.id !== over?.id) {
+      const datas = resumeJsonData.COMPONENTS
+      const oldIndex = datas.findIndex((item: IMATERIALITEM) => item.keyId == active.id)
+      const newIndex = datas.findIndex((item: IMATERIALITEM) => item.keyId == over?.id)
+      dispatch(changeResumeJsonComponentSort(arrayMove(datas, oldIndex, newIndex)))
+    } else {
+      let data = resumeJsonData.COMPONENTS.find((item: IMATERIALITEM) => item.keyId === active.id)
+      let updateData = {
+        cptName: data.model,
+        cptOptionsName: data.cptOptionsName,
+        cptTitle: data.data.title,
+        cptKeyId: data.keyId,
+      }
+      dispatch(updateSelectModel(updateData))
+    }
+  }
 
   return (
     <section>
       {resumeJsonData.LAYOUT === 'classical' && (
         <div className='box-border min-h-290 min-w-205 w-205 bg-white'>
-          {resumeJsonData.COMPONENTS.map((item: any, index: number) => (
-            <div className='list-group-item' key={index}>
-              <ModelBox components={MaterialComponents} item={item} />
-            </div>
-          ))}
+          <DndContext onDragEnd={handleDragEnd}>
+            <i className='i-ant-design-book-outlined i-ant-design-usergroup-add-outlined i-material-symbols-school-outline i-ant-design-trophy-outlined i-material-symbols-edit-note i-material-symbols-sports-baseball-outline i-material-symbols-sports-baseball-outline i-material-symbols-menu-book-outline-rounded i-material-symbols-work-history-outline i-material-symbols-library-books-outline-rounded i-material-symbols-folder-open-outline-rounded display-none'></i>
+            <SortableContext items={resumeJsonData.COMPONENTS.map(i => i.keyId)} strategy={verticalListSortingStrategy}>
+              {resumeJsonData.COMPONENTS.map((item: IMATERIALITEM) => (
+                <div className='list-group-item cursor-move' key={item.keyId}>
+                  <ModelBox components={MaterialComponents} item={item} id={item.keyId} />
+                </div>
+              ))}
+            </SortableContext>
+          </DndContext>
         </div>
       )}
     </section>
